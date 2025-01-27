1f1eb4d79c6e4ab225d3f21d3e6196712b45ec8b
AltinDurmishi <ad70341@ubt-uni.net>
2025-01-27 23:52:53 +0100

    new page sherbime

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ document.addEventListener("DOMContentLoaded", function () {
         navLinks.classList.toggle('active');
     });
 });
+
+
+function toggleContent(button) {
+    const content = button.previousElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    button.textContent = content.style.display === 'block' ? 'LESS' : 'MORE';
+}
+
+
+document.querySelector('.slider-track').addEventListener('animationiteration', () => {
+    const track = document.querySelector('.slider-track');
+    track.style.animation = 'none';
+    void track.offsetHeight; 
+    track.style.animation = 'scroll 20s linear infinite';
+});
+
+
+window.addEventListener('resize', () => {
+    const track = document.querySelector('.slider-track');
+    track.style.animation = 'none';
+    void track.offsetHeight;
+    track.style.animation = 'scroll 20s linear infinite';
+});
